c21c59ad0feae8e9cbb358b5dcb3bffb9b76e544
aleksandar.svorcan <tehnomaniac@gmail.com>
2018-11-29 11:46:38 +0100

    Account image upload fix.

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -10,7 +10,7 @@ def save_image(form_picture):
     random_hex = secrets.token_hex(8)
     _, file_ext = os.path.splitext(form_picture.filename)
     image_name = random_hex + file_ext
-    image_path = os.path.join(current_app.root_path, 'static/profile_pics', image_name)
+    image_path = os.path.join(current_app.root_path, 'static/users/profile_pics', image_name)
     new_size = (300, 300)
     resized_image = Image.open(form_picture)
     resized_image.thumbnail(new_size)
