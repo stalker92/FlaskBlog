3d57a50391fa5e469049b19c92d3b6e9a3d7e55b
aleksandar.svorcan <tehnomaniac@gmail.com>
2018-11-29 10:37:24 +0100

    Database migrations set up.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_migrate import Migrate
 from flaskblog.config import Config
 
 db = SQLAlchemy()
@@ -11,6 +12,7 @@ login_manager = LoginManager()
 login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 mail = Mail()
+migrate = Migrate()
 
 
 def create_app(config_class=Config):
@@ -21,6 +23,7 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+    migrate.init_app(app, db)
 
     from flaskblog.errors.handlers import errors
     from flaskblog.main.routes import main
